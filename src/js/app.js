e33dea057f2f0a53956d62068e476cbd4f27216a
justinrampengan <129474774+justinrampengan@users.noreply.github.com>
2023-03-31 10:02:54 +0700

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -8,11 +8,11 @@ if ('serviceWorker' in navigator) {
     });
 }
 
+let deferredPrompt;
+
 window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent Chrome 67 and earlier from automatically showing the prompt
   e.preventDefault();
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
-  // Update UI notify the user they can add to home screen
-  btnAdd.style.display = 'block';
 });
